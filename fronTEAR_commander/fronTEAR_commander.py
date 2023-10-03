31298da77842f3885859d34077b625484bf38299
Rachel <rachel.chiong@uqconnect.edu.au>
2023-10-03 20:08:44 +1000

    Add more frontier stuff

--- fronTEAR_commander/fronTEAR_commander.py
+++ fronTEAR_commander/fronTEAR_commander.py
@@ -13,7 +13,11 @@ from nav2_msgs.msg import *
 from nav_msgs.msg import OccupancyGrid, Odometry
 from map_msgs.msg import OccupancyGridUpdate
 from visualization_msgs.msg import MarkerArray
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+
+from action_msgs.msg import GoalStatus
+from nav2_msgs.action import FollowWaypoints
+from nav2_msgs.srv import ManageLifecycleNodes, GetCostmap
 
 # External packages (requirements)
 from nav2_simple_commander.robot_navigator import *
@@ -28,6 +32,239 @@ import fronTEAR_commander.nav2_waypoints as nav2_waypoints
 
 OCC_THRESHOLD = 10
 MIN_FRONTIER_SIZE = 5
+
+class Costmap2d():
+    """
+    @reference: https://github.com/SeanReg/nav2_wavefront_frontier_exploration.git
+    """
+    class CostValues(Enum):
+        FreeSpace = 0
+        InscribedInflated = 253
+        LethalObstacle = 254
+        NoInformation = 255
+
+    def __init__(self, map):
+        self.map = map
+
+    def getCost(self, mx, my):
+        return self.map.data[self.__getIndex(mx, my)]
+
+    def getSize(self):
+        return (self.map.metadata.size_x, self.map.metadata.size_y)
+
+    def getSizeX(self):
+        return self.map.metadata.size_x
+
+    def getSizeY(self):
+        return self.map.metadata.size_y
+
+    def __getIndex(self, mx, my):
+        return my * self.map.metadata.size_x + mx
+
+class OccupancyGrid2d():
+    """
+    @reference: https://github.com/SeanReg/nav2_wavefront_frontier_exploration.git
+    """
+    class CostValues(Enum):
+        FreeSpace = 0
+        InscribedInflated = 100
+        LethalObstacle = 100
+        NoInformation = -1
+
+    def __init__(self, map):
+        self.map = map
+
+    def getCost(self, mx, my):
+        return self.map.data[self.__getIndex(mx, my)]
+
+    def getSize(self):
+        return (self.map.info.width, self.map.info.height)
+
+    def getSizeX(self):
+        return self.map.info.width
+
+    def getSizeY(self):
+        return self.map.info.height
+
+    def mapToWorld(self, mx, my):
+        wx = self.map.info.origin.position.x + (mx + 0.5) * self.map.info.resolution
+        wy = self.map.info.origin.position.y + (my + 0.5) * self.map.info.resolution
+
+        return (wx, wy)
+
+    def worldToMap(self, wx, wy):
+        if (wx < self.map.info.origin.position.x or wy < self.map.info.origin.position.y):
+            raise Exception("World coordinates out of bounds")
+
+        mx = int((wx - self.map.info.origin.position.x) / self.map.info.resolution)
+        my = int((wy - self.map.info.origin.position.y) / self.map.info.resolution)
+
+        if  (my > self.map.info.height or mx > self.map.info.width):
+            raise Exception("Out of bounds")
+
+        return (mx, my)
+
+    def __getIndex(self, mx, my):
+        return my * self.map.info.width + mx
+
+
+class FrontierPoint():
+    """
+    Store the frontier coordinate an its classification. Used in FrontierCache.
+    """
+    def __init__(self, x, y):
+        self.classification = 0
+        self.mapX = x
+        self.mapY = y
+
+
+class FrontierCache():
+    """
+    """
+    cache = {}
+
+    def getPoint(self, x, y):
+        idx = self.__cantorHash(x, y)
+
+        if idx in self.cache:
+            return self.cache[idx]
+
+        self.cache[idx] = FrontierPoint(x, y)
+        return self.cache[idx]
+
+    def __cantorHash(self, x, y):
+        return (((x + y) * (x + y + 1)) / 2) + y
+
+    def clear(self):
+        self.cache = {}
+
+class FrontierPoint():
+    def __init__(self, x, y):
+        self.classification = 0
+        self.mapX = x
+        self.mapY = y
+
+def centroid(arr):
+    arr = np.array(arr)
+    length = arr.shape[0]
+    sum_x = np.sum(arr[:, 0])
+    sum_y = np.sum(arr[:, 1])
+    return sum_x/length, sum_y/length
+
+def findFree(mx, my, costmap):
+    fCache = FrontierCache()
+
+    bfs = [fCache.getPoint(mx, my)]
+
+    while len(bfs) > 0:
+        loc = bfs.pop(0)
+
+        if costmap.getCost(loc.mapX, loc.mapY) == OccupancyGrid2d.CostValues.FreeSpace.value:
+            return (loc.mapX, loc.mapY)
+
+        for n in getNeighbors(loc, costmap, fCache):
+            if n.classification & PointClassification.MapClosed.value == 0:
+                n.classification = n.classification | PointClassification.MapClosed.value
+                bfs.append(n)
+
+    return (mx, my)
+
+def getFrontier(pose: PoseStamped, costmap: OccupancyGrid2d):
+    fCache = FrontierCache()
+
+    fCache.clear()
+
+    mx, my = costmap.worldToMap(pose.position.x, pose.position.y)
+
+    freePoint = findFree(mx, my, costmap)
+    start = fCache.getPoint(freePoint[0], freePoint[1])
+    start.classification = PointClassification.MapOpen.value
+    mapPointQueue = [start]
+
+    frontiers = []
+
+    while len(mapPointQueue) > 0:
+        p = mapPointQueue.pop(0)
+
+        if p.classification & PointClassification.MapClosed.value != 0:
+            continue
+
+        if isFrontierPoint(p, costmap, fCache):
+            p.classification = p.classification | PointClassification.FrontierOpen.value
+            frontierQueue = [p]
+            newFrontier = []
+
+            while len(frontierQueue) > 0:
+                q = frontierQueue.pop(0)
+
+                if q.classification & (PointClassification.MapClosed.value | PointClassification.FrontierClosed.value) != 0:
+                    continue
+
+                if isFrontierPoint(q, costmap, fCache):
+                    newFrontier.append(q)
+
+                    for w in getNeighbors(q, costmap, fCache):
+                        if w.classification & (PointClassification.FrontierOpen.value | PointClassification.FrontierClosed.value | PointClassification.MapClosed.value) == 0:
+                            w.classification = w.classification | PointClassification.FrontierOpen.value
+                            frontierQueue.append(w)
+
+                q.classification = q.classification | PointClassification.FrontierClosed.value
+
+
+            newFrontierCords = []
+            for x in newFrontier:
+                x.classification = x.classification | PointClassification.MapClosed.value
+                newFrontierCords.append(costmap.mapToWorld(x.mapX, x.mapY))
+
+            if len(newFrontier) > MIN_FRONTIER_SIZE:
+                frontiers.append(centroid(newFrontierCords))
+
+        for v in getNeighbors(p, costmap, fCache):
+            if v.classification & (PointClassification.MapOpen.value | PointClassification.MapClosed.value) == 0:
+                if any(costmap.getCost(x.mapX, x.mapY) == OccupancyGrid2d.CostValues.FreeSpace.value for x in getNeighbors(v, costmap, fCache)):
+                    v.classification = v.classification | PointClassification.MapOpen.value
+                    mapPointQueue.append(v)
+
+        p.classification = p.classification | PointClassification.MapClosed.value
+
+    return frontiers
+
+
+def getNeighbors(point, costmap, fCache) -> list:
+    """
+    Get adjacent points
+    """
+    neighbors = []
+
+    for x in range(point.mapX - 1, point.mapX + 2):
+        for y in range(point.mapY - 1, point.mapY + 2):
+            if (x > 0 and x < costmap.getSizeX() and y > 0 and y < costmap.getSizeY()):
+                neighbors.append(fCache.getPoint(x, y))
+
+    return neighbors
+
+def isFrontierPoint(point: FrontierPoint, costmap: Costmap, fCache: FrontierCache) -> bool:
+    if costmap.getCost(point.mapX, point.mapY) != OccupancyGrid2d.CostValues.NoInformation.value:
+        return False
+
+    hasFree = False
+    for n in getNeighbors(point, costmap, fCache):
+        cost = costmap.getCost(n.mapX, n.mapY)
+
+        if cost > OCC_THRESHOLD:
+            return False
+
+        if cost == OccupancyGrid2d.CostValues.FreeSpace.value:
+            hasFree = True
+
+    return hasFree
+
+class PointClassification(Enum):
+    MapOpen = 1
+    MapClosed = 2
+    FrontierOpen = 4
+    FrontierClosed = 8
+
 
 def quaternion_to_euler(quaternion):
     import math
@@ -62,23 +299,23 @@ class FronTEARCommander(Node):
 
         # Map which will be used for exploration planning
         # Higher cost - obstacles/walls, lower cost - free space
-        self.costmap = self.create_subscription(
+        self.costmap_sub = self.create_subscription(
                             Costmap,
                             "costmap",
                             self.costmap_callback,
                             10)
-        self.costmap
+        self.costmap_sub
 
         # Incremental updates on costmap
-        self.costmap_updates = self.create_subscription(
+        self.costmap_updates_sub = self.create_subscription(
                                     OccupancyGridUpdate,
                                     "costmap_updates",
                                     self.costmap_updates_callback,
                                     10)
-        self.costmap_updates
+        self.costmap_updates_sub
 
         # Gets Robot's current position
-        self.odom = self.create_subscription(
+        self.odom_sub = self.create_subscription(
             Odometry,
             '/odom',
             self.odom_callback,
@@ -122,8 +359,24 @@ class FronTEARCommander(Node):
         self.pathToPose = 0
         self.same_position_count = 0
 
+        self.waypoints = None
+        self.readyToMove = True
+        self.currentPose = None
+        self.lastWaypoint = None
+
+        self.action_client = ActionClient(self, FollowWaypoints, 'FollowWaypoints')
+        self.initial_pose_pub = self.create_publisher(PoseWithCovarianceStamped,
+                                                      'initialpose', 10)
+
+        self.costmapClient = self.create_client(GetCostmap, '/global_costmap/get_costmap')
+
+        self.costmapSub = self.create_subscription(OccupancyGrid(), '/map', self.occupancyGridCallback, 10)
+        self.costmap = None
         # TODO: Add init code here...
 
+    def occupancyGridCallback(self, msg):
+        self.costmap = OccupancyGrid2d(msg)
+        print(f"Costmap size: {self.costmap.getSize()}, x {self.costmap.getSizeX()}, y {self.costmap.getSizeY()}")
 
     def get_result(self):
         result = self.nav.getResult()
@@ -134,7 +387,6 @@ class FronTEARCommander(Node):
         elif result == TaskResult.FAILED:
             print('Goal failed!')
 
-
     def get_frontier_poses(self) -> PriorityQueue:
         """
         Local costmap is 60 x 60 grid of 0.05 resolution tiles.\n
@@ -143,9 +395,6 @@ class FronTEARCommander(Node):
 
         @Returns: PriorityQueue with best move first
         """
-        frontier = [(10, 10), (10, 30), (10, 50), (30, 10), (30, 50), (50, 10), (50, 30), (50, 50)]
-
-
         cm = self.nav.getLocalCostmap()
         gm = self.nav.getGlobalCostmap()
         print(f"Local costmap resolution: {cm.metadata.resolution}, x: {cm.metadata.size_x}, y: {cm.metadata.size_y} ")
@@ -155,7 +404,7 @@ class FronTEARCommander(Node):
         queue = PriorityQueue()
 
         # Get all possible frontier points
-        f = [(x, y) for x in (5, cm.metadata.size_y - 5) for y in range(5, cm.metadata.size_x, 5)]
+        f = [(x, y) for x in range(5, cm.metadata.size_y - 5) for y in range(5, cm.metadata.size_x, 5)]
         f += [(x, y) for x in range(5, cm.metadata.size_y, 5) for y in range (5, cm.metadata.size_x - 5)]
 
         for el in f:
@@ -172,7 +421,10 @@ class FronTEARCommander(Node):
             for x in range(-5, 5):
                 for y in range(-5, 5):
                     new_index = index + (x * cm.metadata.size_y) + y
-                    total += data[new_index]
+                    if new_index >= cm.metadata.size_y * cm.metadata.size_x:
+                        total += 255
+                    else:
+                        total += data[new_index]
                     count += 1
 
             # Calculate the average cost of moving the robot to that location
@@ -195,6 +447,23 @@ class FronTEARCommander(Node):
         @Returns the best
         """
         waypoints = self.get_frontier_poses()
+        if self.currentPose != None and self.costmap != None:
+            frontiers = getFrontier(self.currentPose, self.costmap)
+            print("Frontier: ", frontiers)
+            location = None
+            largestDist = 0
+            for f in frontiers:
+                dist = math.sqrt(((f[0] - self.currentPose.position.x)**2) + ((f[1] - self.currentPose.position.y)**2))
+                if  dist > largestDist:
+                    largestDist = dist
+                    location = f
+            pose = PoseStamped()
+            pose.pose.position.x = location[0]
+            pose.pose.position.y = location[1]
+            pose.pose.orientation.w = 1.0
+            return pose
+
+
         while not waypoints.empty():
             best_move = waypoints.get()
             if best_move[0] < 50:
@@ -258,8 +527,24 @@ class FronTEARCommander(Node):
 
         return goal
 
+    def setInitialPose(self, pose: list):
+        """
+        Params
+        Pose (x, y): the initial pose to set to give x, y values
+        """
+        self.init_pose = PoseWithCovarianceStamped()
+        self.init_pose.pose.pose.position.x = pose[0]
+        self.init_pose.pose.pose.position.y = pose[1]
+        self.init_pose.header.frame_id = 'map'
+        self.currentPose = self.init_pose.pose.pose
+        self.publishInitialPose()
+        time.sleep(5)
+
+    def publishInitialPose(self):
+        self.initial_pose_pub.publish(self.init_pose)
 
     def odom_callback(self, msg: Odometry):
+        self.currentPose = msg.pose.pose
 
         new_position = msg.pose.pose.position
         new_orientation = msg.pose.pose.orientation
@@ -291,8 +576,8 @@ class FronTEARCommander(Node):
         self.current_orientation = new_orientation
 
         if not isNew and self.same_position_count > 4:
-            new_pose = self.get_best_waypoint()
-            self.send_goal(new_pose)
+            #new_pose = self.get_best_waypoint()
+            #self.send_goal(new_pose)
             self.same_position_count = 0
             return
 
@@ -326,14 +611,14 @@ class FronTEARCommander(Node):
                 if self.current_position == 0:
                     pass
                 new_pose = self.get_best_waypoint()
-                print(f"Sending pose {new_pose}")
                 self.send_goal(new_pose)
         return
 
     def send_goal(self, goal: PoseStamped):
         self.goal_counter += 1
+        print(f"Publishing ({goal.pose.position.x}, {goal.pose.position.y})")
         self.publisher_.publish(goal)
-
+        # self.nav.goToPose(goal)
         while not self.nav.isTaskComplete():
             feedback = self.nav.getFeedback()
         self.get_result()
@@ -357,6 +642,16 @@ class FronTEARCommander(Node):
 def main(args=None):
     rclpy.init(args=args)
     fronTEAR_commander = FronTEARCommander()
+    fronTEAR_commander.setInitialPose([-2.0, -0.5])
+    rclpy.spin_once(fronTEAR_commander, timeout_sec=1.0)
+
+    while fronTEAR_commander.costmap == None:
+        rclpy.spin_once(fronTEAR_commander, timeout_sec=1.0)
+
+    pose = fronTEAR_commander.get_best_waypoint()
+    while pose is not None:
+        fronTEAR_commander.nav.goToPose(pose)
+        pose = fronTEAR_commander.get_best_waypoint()
     rclpy.spin(fronTEAR_commander)
     print("Running FronTEAR Commander...")
 
